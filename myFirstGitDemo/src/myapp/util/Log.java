e510c61937c3c86e24f13c817870362ee1d9a027
Suri <suri_86876200@sina.com>
2013-01-11 21:09:00 +0800

    Signed-off-by: Suri <suri_86876200@sina.com>

--- myFirstGitDemo/src/myapp/util/Log.java
+++ myFirstGitDemo/src/myapp/util/Log.java
@@ -4,5 +4,10 @@ public class Log {
 
 	public void logInfo(){
 		System.out.println("I'm logging!");
+		
+	}
+	
+	public static void main(String args[]){
+		System.out.println("This is main function...");
 	}
 }
