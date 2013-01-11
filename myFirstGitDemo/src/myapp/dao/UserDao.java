5601b1aa5cb55ae170d1cba027958621c142b634
Suri <suri_86876200@sina.com>
2013-01-11 20:25:55 +0800

    Signed-off-by: Suri <suri_86876200@sina.com>

--- myFirstGitDemo/src/myapp/dao/UserDao.java
+++ myFirstGitDemo/src/myapp/dao/UserDao.java
@@ -3,12 +3,16 @@ package myapp.dao;
 public class UserDao {
 
 	public void add(){
-		
+		System.out.println("I'm doing the adding operator");
 	}
 	public void delete(){
 		
 	}
 	public void search(){
 		
+	}
+	public static void main(String args[]){
+		UserDao ud = new UserDao();
+		ud.add();
 	}
 }
